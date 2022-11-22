1bdf6b1e82f868ab8055815f15b59d91eb0ada54
Akpan joseph <akpanjoseph2021@gmail.com>
2022-11-22 13:48:16 +0100

    finished setting up project enviroment

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    color:{
+      Dark_Blue: "hsl(209, 23%, 22%)",
+      Very_Dark_Blue: "hsl(207, 26%, 17%)",
+      Dark_Gray : "hsl(0, 0%, 52%)",
+      Very_Light_Gray : "hsl(0, 0%, 98%)",
+      white : "hsl(0, 0%, 100%)"
+    },
+    screen:{
+      sm: "375px",
+      md: "1440px",
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {},
   },
   plugins: [],
